65a3410b0b74ea3b03e8bb6f9e874b161d1b6227
13141016445@163.com <13141016445@163.com>
2018-03-01 23:10:47 +0800

    change

---
+++ src/pages/home/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+      {{msg}}
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  data () {
+    return {
+      msg: 'header'
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+    .hello{
+        background: red;
+    }
+</style>
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+      <index-header />
+      {{msg}}
+  </div>
+</template>
+
+<script>
+
+import header from "./header.vue";
+
+export default {
+    name: 'HelloWorld',
+    data () {
+        return {
+          msg: 'Welcome to Your Vue.js App'
+        }
+    },
+    components: {
+        "index-header": header
+    }
+
+}
+</script>
+
+
+<style scoped>
+
+</style>
